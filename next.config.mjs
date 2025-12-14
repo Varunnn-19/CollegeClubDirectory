8b38ca983c7326f64f8d3cbbcbd86e022cb3ebc1
Varunnn-19 <varun19082006@gmail.com>
2025-12-14 11:17:53 +0530

    Final

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ const API_SERVER_URL = process.env.API_SERVER_URL || "http://localhost:4000"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
