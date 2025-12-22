caaa05c762940582ba12799308991cb23de29502
Varun Ramesh <varun19082006@gmail.com>
2025-12-22 20:06:31 +0530

    Add PATCH method to CORS configuration

--- server/index.js
+++ server/index.js
@@ -30,8 +30,7 @@ app.use(
       }
       return callback(new Error("Not allowed by CORS"));
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
